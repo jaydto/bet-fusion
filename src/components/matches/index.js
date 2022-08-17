b3aa8f3fa1aacc46770ff41a384ef89b7b92b0ee
jaydto <johnckaris@gmail.com>
2022-08-17 12:37:46 +0300

    updated profile design and betslip on ipad

--- src/components/matches/index.js
+++ src/components/matches/index.js
@@ -156,7 +156,7 @@ const MatchHeaderRow = (props) => {
 
 
     return (
-        <Container className="full-mobile sticky-top" style={{position: "sticky", top: "130px"}}>
+        <Row className="full-mobile sticky-top" style={{position: "sticky", top: "130px"}}>
             <div className="top-matches d-flex position-sticky sticky-top shadow-lg"
                  style={{opacity: "1", top: "100px"}}>
                 <div className="col-sm-2 col-xs-12 pad left-text">
@@ -673,9 +673,6 @@ const MatchRow = (props) => {
                         match?.odds?.away_odd ? <EmptyTextRow odd_key={match?.odd_key}/> : ''
                     }
                 </div>
-
-
-
 
                     {!jackpot && <>
                     {Object.entries(match?.extra_odds || {}).map(([marketName, odds], index) => (

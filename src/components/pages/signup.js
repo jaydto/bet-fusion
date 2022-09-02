ecde381e9b1caa6e4fc2b93490825e45f5e857cc
root <root@bn-dmk-web-1.europe-west1-b.c.betnare.internal>
2022-09-02 10:05:06 +0300

    Fix: Signup Code

--- src/components/pages/signup.js
+++ src/components/pages/signup.js
@@ -105,7 +105,6 @@ const Signup = (props) => {
                         <div className="form-group row d-flex justify-content-left mb-4">
                             <div className="col-md-3">
                                 <button type="submit"
-                                        onClick={submitForm}
                                         className='btn btn-lg btn-primary mt-5 col-md-12 deposit-withdraw-button'>
                                     Signup
                                 </button>

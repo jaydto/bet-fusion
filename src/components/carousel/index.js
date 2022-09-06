d01d0a1d7f746f701e4558e2d1433ed8b7c6ced4
TychaK <kelvinkibugi@gmail.com>
2022-09-06 14:04:36 +0300

    Added 20% deposit on the website

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -3,7 +3,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import {LazyLoadImage} from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
-// import banner4 from '../../assets/img/banner/products/20PercentFirstdepositBonus-min.png'
+import twenty_percent from '../../assets/img/banner/products/20PercentFirstdepositBonus-min.png'
 // import banner2 from '../../assets/img/banner/products/30PercentDAILYCASHBACK.png'
 // import banner3 from '../../assets/img/banner/products/70PercentMULTIBETCASHBACK.png'
 import banner1 from '../../assets/img/banner/products/KaribuBonus.png'
@@ -12,6 +12,7 @@ import odds from '../../assets/img/banner/products/ODDS.png'
 
 const banners = [
     banner1,
+    twenty_percent,
     odds
     // banner2, banner3, banner4, banner5
 ]

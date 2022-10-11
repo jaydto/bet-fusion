3e1fa60f99dec8f4cf60b32ecede9defbc79fdfe
TychaK <kelvinkibugi@gmail.com>
2022-10-11 19:08:51 +0300

    Removing Live Casino Link

--- src/index.js
+++ src/index.js
@@ -116,7 +116,7 @@ render((
                     <Route exact path="/virtuals" element={<Virtuals/>}/>
                     <Route exact path="/livescore" element={<LiveScore/>}/>
                     {/*<Route exact path="/casino" element={<Casino/>}/>*/}
-                    <Route exact path="/live-casino" element={<LiveCasino/>}/>
+                    {/*<Route exact path="/live-casino" element={<LiveCasino/>}/>*/}
                     <Route exact path="/gameplay/:game_id/:live" element={<CasinoGamePlay/>}/>
                     <Route exact path="/highlights" element={<Index/>}/>
                     <Route exact path="/upcoming" element={<Index/>}/>

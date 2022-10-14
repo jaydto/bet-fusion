d1c641fde483138a93688276606ac3f7215ad0ce
kennedywanjahi <kennedywanjahi@gmail.com>
2022-10-14 12:23:47 +0300

    Updating bonus rules implemented on October 14th 2022

--- src/components/right/betslip.js
+++ src/components/right/betslip.js
@@ -23,7 +23,7 @@ const BetSlip = (props) => {
     const [qualifiesBonus, setQualifiesBonus] = useState(false)
 
     const [totalOdds, setTotalOdds] = useState(1);
-    const perSlipBonusOdd = 1.78
+    const perSlipBonusOdd = 2.0
     const maxBonusGames = 4
     //initial betslip loading
     const loadBetslip = useCallback(() => {
@@ -148,7 +148,7 @@ const BetSlip = (props) => {
             message = (`Congratulations, you qualify for bonus. Add ${remainingGames} more game${remainingGames > 1 ? 's' : ''} to place your bet using bonus.`)
 
         } else if ((totalGames === maxBonusGames)) {
-            message = ("Congratulations, you are eligible for a bonus bet. Allowed Bonus Bet Amount is KES 75.")
+            message = ("Congratulations, you are eligible for a bonus bet. Allowed Bonus Bet Amount is KES 100.")
         } else {
             message = ("")
         }

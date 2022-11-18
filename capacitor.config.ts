f0e61bce656069ca4ba2438e604e7dd06d28e7ee
jaydto <johnckaris@gmail.com>
2022-11-18 06:05:03 +0300

    cordova configs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'betnare',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+
 };
+// export interface PluginsConfig {
+//   [CAPACITOR_ANDROID_STUDIO_PATH: string]: | {
+//     [CAPACITOR_ANDROID_STUDIO_PATH: string]: '/Users/apple/Applications/JetBrains Toolbox/Android Studio.app';
+//   }
+//       | undefined;
+// }
 
 export default config;

8732c884d53afd880445b47b71c35c5174b72d49
jaydto <johnckaris@gmail.com>
2022-12-21 08:58:35 +0300

    fast games

--- src/components/pages/virtuals/SpribeGames.js
+++ src/components/pages/virtuals/SpribeGames.js
@@ -9,23 +9,13 @@ import {getFromLocalStorage} from "../../utils/local-storage";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFire} from "@fortawesome/free-solid-svg-icons";
 import {LazyLoadImage} from "react-lazy-load-image-component";
-import dice from "../../../assets/img/fast-games/dice.png"
-import hotline from "../../../assets/img/fast-games/hotline.png"
-import mines from "../../../assets/img/fast-games/mines.png"
-import mini_roulette from "../../../assets/img/fast-games/mini-roulette.png"
-import plinko from "../../../assets/img/fast-games/plinko.png"
+import {Button, ButtonGroup} from "react-bootstrap";
+import SideBar from "../../sidebar/awesome/Sidebar";
 
 
 const GamePlay = (props) => {
     const {game_id, live} = useParams()
 
-    const spribe=[
-        plinko,
-        dice,
-        hotline,
-        mines,
-        mini_roulette
-    ]
 
     const [games, setGames] = useState([])
 
@@ -50,16 +40,34 @@ const GamePlay = (props) => {
     useEffect(() => {
         getFastGames()
     }, [])
+
+    const getFastGamesImages = (nare_games, folder = 'fast-games') => {
+
+        let nare_image;
+        try {
+
+            nare_image = require(`../../../../src/assets/img/${folder}/${nare_games.toLowerCase()}.png`);
+
+        } catch (error) {
+            console.log("error",error)
+
+        }
+        return nare_image
+    }
+
     return (
         <>
             <Header/>
             <div className="amt">
-                <div className="d-flex flex-row justify-content-between">
+                <div className="d-flex flex-row ">
+                    <SideBar loadCompetitions/>
+                    <div className="gz home" style={{width: '100%'}}>
+                        <div className="col-md-12 d-flex flex-column">
                     <div className="col-md-12">
                         <div className="homepage">
-                            <div className={'row text-white p-2 shadow-sm mt-2'}>
+                            <div className={'row row-cols-4 text-white p-2 shadow-sm mt-2'}>
                                 {gamesLoaded && games?.map((game) => (
-                                    <div className={'col-md-2 cursor-pointer'}>
+                                    <div className={'col cursor-pointer'}>
                                         <div
                                             className={'mt-1 mb-1 d-flex flex-column shadow-lg virtual-game-container'}>
                                             <Link to={`/nare-games/${game?.key}`}
@@ -68,11 +76,23 @@ const GamePlay = (props) => {
                                                 <p className={'text-center bold text-elipsis text-uppercase'}>
                                                     {game?.name}
                                                 </p>
+                                                <LazyLoadImage
+                                                    src={getFastGamesImages(game.name)}
+                                                    alt=""
+                                                     alt="#"
+                                                />
+                                                <div className="overlay shadow-sm row">
+                                                        <Button variant="warning">
+                                                            Play Game
+                                                        </Button>
+                                                </div>
                                             </Link>
                                         </div>
                                     </div>
                                 ))}
                             </div>
+                        </div>
+                    </div>
                         </div>
                     </div>
                 </div>

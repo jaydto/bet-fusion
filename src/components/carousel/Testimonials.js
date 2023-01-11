113cec10ec3fe7a1774b5d51924da5439a806ff9
jaydto <johnckaris@gmail.com>
2023-01-11 10:06:14 +0300

    winners text change to recent winners

--- src/components/carousel/Testimonials.js
+++ src/components/carousel/Testimonials.js
@@ -30,7 +30,7 @@ const Testimonials = () => {
 
             <div className=" testimonials-style border border-warning d-flex sticky-testimony">
 
-                <div className={'text-warning bg-black size-1'} style={{zIndex:"200"}}>Winners</div>
+                <div className={'text-warning bg-black size-1'} style={{zIndex:"200", whiteSpace:'nowrap'}}>Recent Winners</div>
 
                 <ul className={' d-flex text-light flex-nowrap testimonial-style size-2'}  >
                     {testimonials?.map((testimony,index)=>(

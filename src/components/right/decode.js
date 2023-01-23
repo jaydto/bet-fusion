ca1e7e614eef6da52f7e5a9fb99ba8c05536828c
TychaK <kelvinkibugi@gmail.com>
2023-01-23 19:10:13 +0300

    Added logic to track number of times shared betslip has actually been placed.

--- src/components/right/decode.js
+++ src/components/right/decode.js
@@ -5,7 +5,7 @@ import Notify from "../utils/Notify";
 import {Spinner} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faShare} from "@fortawesome/free-solid-svg-icons";
-
+import {setLocalStorage} from "../utils/local-storage";
 
 
 const DecodeCode = () => {
@@ -14,17 +14,16 @@ const DecodeCode = () => {
     useEffect(() => {
         inputRef.current = document.getElementById("code");
     }, []);
-    const [loading, setLoading]=useState(false)
-    const [code, setCode]=useState("")
+    const [loading, setLoading] = useState(false)
+    const [code, setCode] = useState("")
     const handleChanges = (e) => {
         e.preventDefault()
-        setCode( e.target.value);
+        setCode(e.target.value);
     }
 
     const handleslip = async (share_code) => {
 
         let endpoint = "/v1/bs-decode"
-        console.log("data_slip", share_code)
 
         let data = {
             "betslip_share_code": share_code
@@ -33,17 +32,17 @@ const DecodeCode = () => {
         await makeRequest({url: endpoint, method: "POST", data: data}).then(([status, result]) => {
             // console.log(result?.success)
 
-            if(status==200){
+            if (status == 200) {
                 Object.entries(result?.success).map(([match_id, match]) => {
                     match.live = Number(match?.live) !== 0
                     match.bet_type = String(match?.bet_type)
                     addToSlip(match)
                 })
 
-                        window.location.href="/"
+                setLocalStorage('betslip_share_code', share_code)
+                window.location.href = "/"
 
-            }
-            else{
+            } else {
                 Notify(
                     {status: 400, message: result?.error, token: ""}
                 )
@@ -56,39 +55,40 @@ const DecodeCode = () => {
     }
 
 
-
-
     return (
 
         <React.Fragment>
             <div className=" ">
                 <div className="card card-radius profile-bg text-light p-0 mt-2">
-                    <div className="card-body p-3" style={{overflow:"hidden"}}>
-                        <form  >
+                    <div className="card-body p-3" style={{overflow: "hidden"}}>
+                        <form>
                             <div className="form-group row d-flex justify-content-center ">
                                 <div className="col-md-12">
-                                    <label className={"text-bold h4 text-center"}> Enter your betslip share code to load betslip </label>
+                                    <label className={"text-bold h4 text-center"}> Enter your betslip share code to load
+                                        betslip </label>
                                     <div className={"d-flex flex-column"}>
                                         <input
                                             className="text-dark deposit-input form-control col input-field"
                                             id="code"
                                             ref={inputRef}
-                                            onChange={(e)=>handleChanges(e)}
+                                            onChange={(e) => handleChanges(e)}
                                             name="code"
                                             type="text"
                                             value={code}
-                                            style={{borderRadius:"0.3rem"}}
+                                            style={{borderRadius: "0.3rem"}}
                                             autoFocus={true}
                                             placeholder='eg. PWXfsxR'
                                         />
-                                        <div className="form-group row d-flex justify-content-left col mt-4" style={{whiteSpace:"nowrap"}}>
+                                        <div className="form-group row d-flex justify-content-left col mt-4"
+                                             style={{whiteSpace: "nowrap"}}>
                                             <div className=" d-flex align-items-start">
-                                                <button type={"button"} onClick={()=>handleslip(code)}
-                                                        className='btn btn-lg  w-100 deposit-withdraw-button text-white d-flex align-items-center justify-content-center' style={{backgroundColor:"#527994", borderRadius:"0.3rem"}}>
+                                                <button type={"button"} onClick={() => handleslip(code)}
+                                                        className='btn btn-lg  w-100 deposit-withdraw-button text-white d-flex align-items-center justify-content-center'
+                                                        style={{backgroundColor: "#527994", borderRadius: "0.3rem"}}>
                                                     <strong>
                                                         LOAD SLIP
-                                                        </strong>&nbsp;
-                                                    {loading&&<div className={` text-white d-block`}>
+                                                    </strong>&nbsp;
+                                                    {loading && <div className={` text-white d-block`}>
                                                         <Spinner animation={'grow'} size={'sm'}/>
                                                     </div>}
                                                 </button>

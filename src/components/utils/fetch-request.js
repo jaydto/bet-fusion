24a27ec3a649d74aac9f84b0d0f12ad381643a5f
TychaK <kelvinkibugi@gmail.com>
2023-01-27 20:22:03 +0300

    Updated tag css font size to 12px, fetch request modified to api

--- src/components/utils/fetch-request.js
+++ src/components/utils/fetch-request.js
@@ -3,7 +3,7 @@ import {setLocalStorage, getFromLocalStorage} from './local-storage';
 const ENC_KEY = '2bdVweTeI42s5mkLdYHyklTMxQS5gLA7MDS6FA9cs1uobDXeruACDic0YSU3si04JGZe4Y';
 const BASE_URL = 'https://api.betnare.com';
 // const BASE_URL = 'http://127.0.0.1:5000';
-
+//
 const makeRequest = async ({url, method, data = null, use_jwt = false}) => {
 
     url = BASE_URL + url;

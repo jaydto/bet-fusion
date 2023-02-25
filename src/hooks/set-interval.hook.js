a8cd9daba75115f989f9089d7049527f079652c9
jaydto <johnckaris@gmail.com>
2023-02-25 10:32:41 +0300

    useInterval updated

--- src/hooks/set-interval.hook.js
+++ src/hooks/set-interval.hook.js
@@ -1,20 +1,38 @@
-import { useMemo, useRef } from 'react';
+import {useEffect, useMemo, useRef} from 'react';
+import useIsomorphicLayoutEffect from 'use-isomorphic-layout-effect';
 
 const useInterval = (callback, delay) => {
-  const savedCallback = useRef();
+  const savedCallback = useRef(callback);
 
-  useMemo(() => {
-    savedCallback.current = callback;
-  }, [callback]);
+  // useMemo(() => {
+  //   savedCallback.current = callback;
+  // }, [callback]);
+  //
+  // useMemo(() => {
+  //   function tick() {
+  //     savedCallback.current();
+  //   }
+  //
+  //   if (delay !== null) {
+  //     let id = setInterval(tick, delay);
+  //     return () => clearInterval(id);
+  //   }
+  // }, [delay]);
 
-  useMemo(() => {
-    function tick() {
-      savedCallback.current();
+  useIsomorphicLayoutEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    // Don't schedule if no delay is specified.
+    // Note: 0 is a valid value for delay.
+    if (!delay && delay !== 0) {
+      return
     }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
+
+    const id = setInterval(() => savedCallback.current(), delay)
+
+    return () => clearInterval(id)
+  }, [delay])
 }
 export default useInterval;

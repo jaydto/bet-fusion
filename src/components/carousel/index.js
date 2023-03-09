7896f3c021fa95834747d31e82f714ece9c2add4
jaydto <johnckaris@gmail.com>
2023-03-09 10:39:34 +0300

    banner and jackpot stake

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -9,7 +9,7 @@ import stakeBooster from '../../assets/img/banner/products/Bet_Nare_20_Stake_Boo
 import dailyDepositGift from '../../assets/img/banner/products/Bet_Nare_20_gift.webp'
 import karibuGiftWallet from "../../assets/img/banner/products/Bet_Nare_3000_karibu_gift.webp"
 import Aviator from "../../assets/img/banner/products/Aviator.webp"
-import jackpot from "../../assets/img/banner/products/Bet_Nare_300k_Jackpot_New.webp"
+import jackpot from "../../assets/img/banner/products/HalfMilliJP.webp"
 
 
 

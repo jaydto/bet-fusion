9467619af8ddecda94ad4a3d25ae52d3776b1e24
jaydto <johnckaris@gmail.com>
2023-04-17 17:02:50 +0300

    font-size

--- src/components/pages/smart-soft/SmartPlay.js
+++ src/components/pages/smart-soft/SmartPlay.js
@@ -93,7 +93,7 @@ const SmartPlay = (props) => {
                                         src={gameUrl} title="Gadme" width={'100%'} height={'600px'}></iframe>
                             </>}
                             {pathname.includes("JetX")&&
-                                <div className={'card rounded-3 e '} style={{color:"#999",background:"transparent", textDecoration:"none", listStyle:"none"}}>
+                                <div className={'card rounded-3 e '} style={{color:"#999",background:"transparent", textDecoration:"none", listStyle:"none",fontSize:'11px'}}>
                                     <div className={'card-body p-3'}>
                                         <h3 className={'text-center text-warning flashy'}>WIN Upto 2,500,000/= with JetX the Money Multiplier daily </h3>
                                         <ul>

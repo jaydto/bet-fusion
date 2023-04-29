c9d03ddc2b5fbd8ceb98aca8e32e1a09bf63d693
betnare <110972523+betnare@users.noreply.github.com>
2023-04-29 16:01:55 +0300

    Update Withdraw.js
    Correct message on withdrawal min limit

--- src/components/pages/deposit-withraw/Withdraw.js
+++ src/components/pages/deposit-withraw/Withdraw.js
@@ -40,7 +40,7 @@ const Withdrawal = (props) => {
         }
     //Removed the  upper limit  values.amount > 70000
         if (!values.amount || values.amount < 50 ) {
-            errors.amount = "Please enter amount between KES 50 and KES 70, 000";
+            errors.amount = "Please enter amount above KES 50";
         }
         return errors
     }

b3f834a9e8f984fc706deb33b2f907f21c5f2eab
jaydto <johnckaris@gmail.com>
2023-05-11 13:17:16 +0300

    banners 2000x356 added

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -3,31 +3,16 @@ import Carousel from 'react-bootstrap/Carousel';
 import {LazyLoadImage} from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
+import Av2 from '../../assets/img/banner/products/Aviator.webp'
+import Roma from '../../assets/img/banner/products/RomavsLeverkusen.webp'
 
-import cashback from '../../assets/img/banner/products/Bet_Nare_100_Cashback.webp'
-import stakeBooster from '../../assets/img/banner/products/Bet_Nare_20_Stake_Booster.webp'
-import dailyDepositGift from '../../assets/img/banner/products/Bet_Nare_20_gift.webp'
-import karibuGiftWallet from "../../assets/img/banner/products/Bet_Nare_3000_karibu_gift.webp"
-import Aviator from "../../assets/img/banner/products/Aviator.webp"
-import jackpot from "../../assets/img/banner/products/HalfMilliJP.webp"
-import Drops from "../../assets/img/banner/products/DropsAndWins Web.webp"
-import Drops260 from  '../../assets/img/banner/products/Pragmatic260.webp'
-import Drops65 from '../../assets/img/banner/products/pragmatic65.webp'
-import Miller from '../../assets/img/banner/products/MilanvsInter.webp'
-import Av2 from '../../assets/img/banner/products/Aviator2.webp'
-import Juventus from '../../assets/img/banner/products/JuventusvsSevilla.webp'
+
 
 const banners = [
-    {src: Juventus, url: "/"},
-    {src: Miller, url: "/promotions"},
+
+    {src: Roma, url: "/match/32870895"},
     {src: Av2, url: "/nare-games/aviator"},
-    // {src: Aviator, url: "/nare-games/aviator"},
-    // {src: karibuGiftWallet, url: "/promotions"},
-    // {src: jackpot, url: "/jackpot"},
-    // {src: stakeBooster, url: "/promotions"},
-    // {src: dailyDepositGift, url: "/deposit"},
-    // {src: cashback, url: "/promotions"},
-    // {src: Drops, url: "/casino"}
+
 ]
 
 const CarouselLoader = (props) => {
@@ -39,9 +24,9 @@ const CarouselLoader = (props) => {
     return (
         <Carousel indicators={false}>
             {banners.map((banner, idx) => (
-                <Carousel.Item key={idx} className={'banner-height'}>
+                <Carousel.Item key={idx} >
                     <LazyLoadImage
-                        className="d-block w-100 cursor-pointer banner-height"
+                        className="d-block w-100 cursor-pointer"
                         style={{display: imageLoaded ? 'block' : 'none'}}
                         src={banner.src}
                         onLoad={onImageLoaded}

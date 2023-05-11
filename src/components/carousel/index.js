0de2eb5179ad8330f8193af0bf2ad7b854988f38
TychaK <kelvinkibugi@gmail.com>
2023-05-11 12:40:27 +0300

    Removing Milan banner

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -10,11 +10,9 @@ import dailyDepositGift from '../../assets/img/banner/products/Bet_Nare_20_gift.
 import karibuGiftWallet from "../../assets/img/banner/products/Bet_Nare_3000_karibu_gift.webp"
 import Aviator from "../../assets/img/banner/products/Aviator.webp"
 import jackpot from "../../assets/img/banner/products/HalfMilliJP.webp"
-import Millan from "../../assets/img/banner/products/MilanvsInter.webp"
 
 
 const banners = [
-    {src: Millan, url: "/match/32868827"},
     {src: Aviator, url: "/nare-games/aviator"},
     {src: karibuGiftWallet, url: "/promotions"},
     {src: jackpot, url: "/jackpot"},

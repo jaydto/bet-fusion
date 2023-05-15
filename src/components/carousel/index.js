9d507c3cc87ca8af71d3880df947be266c1674ae
jaydto <johnckaris@gmail.com>
2023-05-15 13:58:14 +0300

    ch-banner-liverpool

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -10,9 +10,11 @@ import dailyDepositGift from '../../assets/img/banner/products/Bet_Nare_20_gift.
 import karibuGiftWallet from "../../assets/img/banner/products/Bet_Nare_3000_karibu_gift.webp"
 import Aviator from "../../assets/img/banner/products/Aviator.webp"
 import jackpot from "../../assets/img/banner/products/HalfMilliJP.webp"
+import liverpool from "../../assets/img/banner/products/LeicestervsLiverpool.webp"
 
 
 const banners = [
+    {src: liverpool, url: "/match/32892171"},
     {src: Aviator, url: "/nare-games/aviator"},
     {src: karibuGiftWallet, url: "/promotions"},
     {src: jackpot, url: "/jackpot"},

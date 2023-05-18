b61d99b3e13eb7d7213df424f1c36b3136e881a7
jaydto <johnckaris@gmail.com>
2023-05-18 17:53:49 +0300

    error handling for an empty slip

--- src/components/betslip/BetslipShareDecode.js
+++ src/components/betslip/BetslipShareDecode.js
@@ -19,21 +19,30 @@ const BetslipShareDecode = () => {
         let data = {
             "betslip_share_code": share_code
         }
-        let message={status: 401, message: 'Kindly check your slip and try again', token: ''}
-        await makeRequest({url: endpoint, method: "POST", data: data}).then(([status, result]) => {
-            // console.log(result?.success)
-            if(status==200){
-                Object.entries(result?.success).map(([match_id, match]) => {
-                    match.live = Number(match?.live) !== 0
-                    match.bet_type = String(match?.bet_type)
-                    addToSlip(match)
-                })
-                // setLocalStorage('betslip', (result?.success), 1 * 60 * 60 * 1000);
-            }else{
-                setLocalStorage("betslip-share-code-invalid",message)
-            }
+        let message= {status: 401, message: 'Betslip share code is required', token: ''}
 
-        });
+        if(share_code.length>0){
+            let message={status: 401, message: 'Kindly check your slip and try again', token: ''}
+            await makeRequest({url: endpoint, method: "POST", data: data}).then(([status, result]) => {
+                // console.log(result?.success)
+                if(status==200){
+                    Object.entries(result?.success).map(([match_id, match]) => {
+                        match.live = Number(match?.live) !== 0
+                        match.bet_type = String(match?.bet_type)
+                        addToSlip(match)
+                    })
+                    // setLocalStorage('betslip', (result?.success), 1 * 60 * 60 * 1000);
+                }else{
+                    Notify(message)
+                    setLocalStorage("betslip-share-code-invalid",message)
+                }
+
+
+
+            });
+        }else{
+            setLocalStorage("betslip-share-code-invalid",message)
+        }
     }
 
     useEffect(() => {

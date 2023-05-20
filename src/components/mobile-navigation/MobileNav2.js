cd6700880d854ff3e9a8e0f2705795085f9f55bd
jaydto <johnckaris@gmail.com>
2023-05-20 16:24:22 +0300

    navigation to top league on mobile

--- src/components/mobile-navigation/MobileNav2.js
+++ src/components/mobile-navigation/MobileNav2.js
@@ -5,11 +5,7 @@ import {LazyLoadImage} from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import {getFromLocalStorage, setLocalStorage} from "../utils/local-storage";
 import makeRequest from "../utils/fetch-request";
-// import worldCup from "../../../src/assets/img/leagues/worldcup.png"
 import useAnalyticsEventTracker from "../analytics/useAnalyticsEventTracker";
-import {Dropdown, DropdownButton} from "react-bootstrap";
-import DropdownItem from "react-bootstrap/DropdownItem";
-
 
 const MobileNav2 = (props) => {
 
@@ -70,35 +66,18 @@ const MobileNav2 = (props) => {
             abortController.abort();
         };
     }, []);
-    // console.log('sports',sport)
 
 
-    const default_img = 'default_sport';
-    let sport_image;
-
-    try {
-        sport_image = require(`../../assets/svg/${sport?.sport_name}.svg`);
-    } catch (error) {
-        sport_image = require(`../../assets/svg/${default_img}.svg`);
-    }
-    const getDefaultMarketsForSport = (allsports) => {
-        return allsports?.default_display_markets
-    }
 
 
     return (<div className={"league-container"}>
-        {/*{sport==null?setSport(getFromLocalStorage ("categories")):""}*/}
         <table  style={{width: "100%", textAlign: "center"}}>
             <tbody>
             <tr className={"d-flex league-row-mobile"}>
                 {sport?.top_soccer.map((top_league, index) => (
                     <td key={index} className={` d-flex menu-t sport-check ${pathname===top_league.competition_id?" active":""}`} style={{paddingLeft: "4px",textAlign: 'center',lineHeight: '1.5'}}>
-                        <Link  style={{paddingRight: "4px",width:"max-content"}}  to={{pathname: `/competition`,search: `competitionid=${top_league.competition_id}&sub_type_id=1`}}>
-                            {/*<div className="menu-img">*/}
-                            {/*    */}
-                            {/*</div>*/}
-                            {/*`/competition/${top_league.sport_id}/${top_league.category_id}/${top_league.competition_id}?sport_id=79&sub_type_id=1`*/}
-                            <div className="inner-div active d-flex align-items-center justify-content-center">
+                        <Link  style={{paddingRight: "4px",width:"max-content"}}  to={ `/competition/79/${top_league.category_id}/${top_league.competition_id}?sport_id=79&sub_type_id=1`}>
+                         <div className="inner-div active d-flex align-items-center justify-content-center">
                                 <LazyLoadImage
                                     className="side-icon"
                                     src={getSportImageIcon(top_league.competition_name)}

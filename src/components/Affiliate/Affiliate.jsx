9ab4d41da8353cc4e3c8a7310729855ea965c2f3
jaydto <johnckaris@gmail.com>
2023-05-25 12:18:25 +0300

    wapaffiliate

---
+++ src/components/Affiliate/Affiliate.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+const Affiliate = () => {
+
+	return (
+		<>
+			<header>
+
+			</header>
+			<section className={'hero-banner'}>
+
+			</section>
+			<section className={'affiliate-info'}>
+
+			</section>
+			<footer>
+
+			</footer>
+		</>
+	)
+}
+export default React.memo(Affiliate)

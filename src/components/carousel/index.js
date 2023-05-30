0e6d1bbeb40bbd26a894367227f63b11c63b27ca
jaydto <johnckaris@gmail.com>
2023-05-30 21:14:08 +0300

    banners from api

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,28 +1,27 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import {LazyLoadImage} from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
-
-import instant from '../../assets/img/banner/products/3kinstant.webp'
-import bonus from '../../assets/img/banner/products/20Bonus.webp'
-import affiliate from '../../assets/img/banner/products/Affiliate.webp'
-import app from '../../assets/img/banner/products/App.webp'
-import casino from '../../assets/img/banner/products/Casino_banner.webp'
-import spaceman from '../../assets/img/banner/products/SpaceMan.webp'
-import jackpot from '../../assets/img/banner/products/jackpot.webp'
-const banners = [
-    {src: instant, url: "/promotions"},
-    {src: bonus, url: "/promotions"},
-    {src: affiliate, url: "/"},
-    {src: app, url: "/app"},
-    {src: casino, url: "/casino"},
-    {src: jackpot, url: "/jackpot"},
-    {src: spaceman, url: "/gameplay/1301/1"},
-
-
-]
+import makeRequest from "../utils/fetch-request";
 
 const CarouselLoader = (props) => {
+    const [banners, setBanners] = useState([])
+    const getCarouselImages = async () => {
+        let endpoint = "/v1/carousel-images"
+        const [carousel_results] = await Promise.all([
+            makeRequest({url: endpoint, method: "GET"})
+        ]);
+        let [status, carousel_result] = carousel_results;
+
+        if (status === 200) {
+            setBanners(carousel_result?.images)
+        }
+    }
+
+    useEffect(() => {
+        getCarouselImages()
+    }, [])
+
     const [imageLoaded, setImageLoaded] = useState(false);
     const onImageLoaded = () => {
         setImageLoaded(true);
@@ -31,16 +30,17 @@ const CarouselLoader = (props) => {
     return (
         <Carousel indicators={false}>
             {banners.map((banner, idx) => (
-                <Carousel.Item key={idx} >
+                <Carousel.Item key={idx}>
                     <LazyLoadImage
+                        title={banner?.title}
                         className="d-block w-100 cursor-pointer"
                         style={{display: imageLoaded ? 'block' : 'none'}}
-                        src={banner.src}
+                        src={banner?.image_url}
                         onLoad={onImageLoaded}
                         alt="Batnare"
                         effects="blur"
                         onClick={() => {
-                            window.location.href = banner.url
+                            window.location.href = banner?.link_url
                         }}
                     />
                 </Carousel.Item>

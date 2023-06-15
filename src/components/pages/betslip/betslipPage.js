75ef2f1ed602cccdeeebf95bf2115ad624dd1185
jaydto <johnckaris@gmail.com>
2023-06-15 14:43:34 +0300

    updated balance check and alert position for placebet

--- src/components/pages/betslip/betslipPage.js
+++ src/components/pages/betslip/betslipPage.js
@@ -19,13 +19,12 @@ import {faCloudDownloadAlt} from "@fortawesome/free-solid-svg-icons/faCloudDownl
 import {faCoins} from "@fortawesome/free-solid-svg-icons/faCoins";
 import KironSlip from "../../right/kironslip";
 import {formatNumber} from "../../utils/betslip";
+import Header from "../../header/header";
 
 const BetslipPage = () => {
-  const { height, width } = useWindowDimensions();
-  const gaEventTracker = useAnalyticsEventTracker('Navigation');
   const [tab, setTab] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [user, ] = useState(getFromLocalStorage("user"));
+
   let url = new URL(window.location);
   const jp = url.searchParams.get("jackpot");
   const nL=url.searchParams.get("nare-league")
@@ -81,104 +80,7 @@ const BetslipPage = () => {
 
   return (
       <>
-
-        <Navbar expand="lg" className="mb-0 ck pc os app-navbar top-nav header-mobile-kiron" fixed="top" variant="dark">
-          <Container fluid className={'d-flex justify-content-between mobile-change'}>
-            <Navbar.Brand className={`e logo align-self-start menu-control d-flex justify-content-between w-100`} title="Betnare">
-              <Link to={{pathname: "/"}} className="col-4 mt-3 resize-mobile" style={{ marginLeft:"-5px"}}>
-                <img src={logo} alt="Betnare" title="Betnare" effects="blur"
-                     className={"image-size "} style={user?{marginBottom:"0px" }:{marginBottom:"11px", width:'auto'}}/>
-              </Link>
-
-              {user &&
-                  <div
-                      className="col-md-6  d-flex  right justify-content-end align-items-center w-change2 gap-2 ipad-show"
-                      style={{marginLeft: 'auto'}}>
-                    <div>
-                      <Link
-                          to={{pathname: "/deposit"}}
-                          className={"deposit-button size-font-user-action remove-from-small-screen"}>
-                                          <span className="">
-                                           <span className=" "> <FontAwesomeIcon
-                                               icon={faCloudDownloadAlt}/></span>&nbsp;
-                                            DEPOSIT
-                                          </span>
-                      </Link>
-                    </div>
-                    <div>
-
-                      <div
-                          className={"deposit-button size-font-user-action d-flex align-items-center"}
-                          style={{marginRight: "12px"}}>
-                                          <span className="text-warning">
-                                           <span className=" "><FontAwesomeIcon icon={faCoins} className={"text-warning"}/>
-                                               </span>&nbsp;
-                                            KSH {formatNumber(user.balance) || 0}
-                                          </span>
-                      </div>
-                    </div>
-
-                    <div className="col-1 button-toggle space-button"
-                         style={{width: "4.1rem", overflowY: "auto", marginLeft: '0px'}}>
-                      <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${"lg"}`}
-                                     className="px-3 py-3" onClick={toggle}/>
-                    </div>
-                  </div>}
-              <>
-                {!user&&<div className="col-sm-2 mobile-profile1 align-items-center gap-3 ipad-show" style={{marginLeft:'auto'}}>
-                  <div className="remove-verify remove-verify-on-small">
-                    <Link className="cg  login-color login-size btn bg-success text-light"
-                          to={"/verify"} title="Verify Account"
-                          onClick={() => gaEventTracker('Verify')}>
-                      <span className="register-label text-light">Verify</span>
-                    </Link>
-                  </div>
-                  <div className="">
-                    <Link className="cg  login-color login-size btn bg-warning text-light"
-                          to={"/signup"} title="Join now"
-                          onClick={() => gaEventTracker('Register')}>
-                      <span className="text-light ">Register</span>
-                    </Link>
-                  </div>
-
-                  <Link to={"/login"} className="cg  login-color login-size btn" type="submit">
-                    <span>Login</span>
-                  </Link>
-
-                  <div className="col-1 button-toggle space-button" style={{width: "4.1rem", overflowY:"auto",marginLeft:'0px'}}>
-                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${"lg"}`} className="px-3 py-3" onClick={toggle} />
-                  </div>
-
-                </div>}
-              </>
-
-
-            </Navbar.Brand>
-
-
-            <Navbar.Offcanvas
-                style={{width: "80%", height: "100%",zIndex: "9999", marginTop: "0px",overflowY:"auto"}}
-                className='off-canvas background-primary p-0'
-                id={`offcanvasNavbar-expand-${expand}`}
-                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                placement="start">
-              <Offcanvas.Header closeButton className='text-white' closeVariant={"white"}>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  <div className="col-3">
-                    <div>
-                      <LazyLoadImage src={logo} alt="Betnare" title="Betnare" effects="blur"/>
-                    </div>
-                  </div>
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body className={(width<=575?user?"":"":"")}>
-                <SidebarMobile/>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-
-
+        <Header slip={true}/>
         <div
             className=" "
             style={{ overflow:'hidden' }}>

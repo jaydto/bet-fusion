b458e4a9a6b210a79bb148a7980f9da19d8da8c7
jaydto <johnckaris@gmail.com>
2023-06-23 13:17:32 +0300

    filter bet history

--- src/components/modals/FilterBetHistory.js
+++ src/components/modals/FilterBetHistory.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { ToastContainer, Modal, Button, Form } from 'react-bootstrap';
 import "./modals-custom.css"
-const GameHistoryList = ({ games }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [filterCategory, setFilterCategory] = useState('');
+import {Context} from "../../context/store";
+const GameHistoryList = (props) => {
+    const {visible, games,setShowGameFilter} = props
+    const [isOpen, setIsOpen] = useState(visible)
+    const [state, dispatch]=useContext(Context)
+    const [message, setMessage] = useState(null);
+    const [formData, setFormData] = useState({});
+    const [selectedFilter, setSelectedFilter] = useState('today');
+
+    console.log("dates", games)
 
     const handleOpenModal = () => {
         setIsOpen(true);
@@ -13,33 +20,56 @@ const GameHistoryList = ({ games }) => {
         setIsOpen(false);
     };
 
+    const hideModal = () => {
+        setIsOpen(false)
+        setShowGameFilter(false)
+        // setLocalStorage("share-modal",0)
+        // console.log("share_from_modal",getFromLocalStorage("share-modal"))
+    }
+
     const handleFilterChange = (category) => {
-        setFilterCategory(category);
+        setSelectedFilter(category);
+    };
+
+    const currentDate = new Date();
+    const isSameDate = (date1, date2) => {
+        return (
+            date1.getFullYear() === date2.getFullYear() &&
+            date1.getMonth() === date2.getMonth() &&
+            date1.getDate() === date2.getDate()
+        );
     };
 
     const filterGames = () => {
-        let filteredGames = games;
+        console.log("selectedFilter",selectedFilter )
+        const filteredGames = games?.filter((game) => {
+            const createdDate = new Date(game.created);
+            if (selectedFilter === 'today') {
+                return isSameDate(createdDate, currentDate);
+            } else if (selectedFilter === 'yesterday') {
+                const yesterday = new Date(currentDate);
+                yesterday.setDate(currentDate.getDate() - 1);
+                return isSameDate(createdDate, yesterday);
+            } else if (selectedFilter === 'week') {
+                const oneWeekAgo = new Date(currentDate);
+                oneWeekAgo.setDate(currentDate.getDate() - 7);
+                return createdDate >= oneWeekAgo && createdDate <= currentDate;
+            } else if (selectedFilter === 'month') {
+                const oneMonthAgo = new Date(currentDate);
+                oneMonthAgo.setMonth(currentDate.getMonth() - 1);
+                return createdDate >= oneMonthAgo && createdDate <= currentDate;
+            }
+            return false;
+        });
+        console.log("filteredByDatesGames",filteredGames )
 
-        if (filterCategory === 'today') {
-            // Filter games for today
-            // ...
-        } else if (filterCategory === 'yesterday') {
-            // Filter games for yesterday
-            // ...
-        } else if (filterCategory === 'week') {
-            // Filter games for the current week
-            // ...
-        } else if (filterCategory === 'month') {
-            // Filter games for the current month
-            // ...
-        }
-
-        return filteredGames?.map((game) => (
-            <div key={game.bet_id}>
-                {/* Render game details here */}
-            </div>
-        ));
+        dispatch({type: "SET", key: "bets_by_date", payload: filteredGames });
     };
+
+    // handleFilterChange
+    useEffect(()=>{
+        filterGames()
+    },[selectedFilter])
 
     const handleSubmit = () => {
         // Handle login form submission
@@ -59,20 +89,38 @@ const GameHistoryList = ({ games }) => {
                 style={{ zIndex: '9999' }}
             >
                 {/* Modal contents */}
+                {/* Filter buttons */}
+                <Modal.Header closeButton={false} className={"w-100"} style={{borderBottom:"0px"}}>
+                    <Modal.Title className={"w-100"}>
+                        <div className={"d-flex justify-content-between align-items-start flex-column px-4"}>
+                            <div className="close-history-filter">
+                                <input
+                                    id={"history-filter"}
+                                    type="submit"
+                                    value="X"
+                                    onClick={hideModal}
+                                />
+                            </div>
+                            <strong style={{width:"100%", fontSize:"19px", fontWeight:"bolder", letterSpacing:"2px"}}>Filters</strong>
+                        </div>
+
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{borderBottom:"0px", paddingTop:"4px", paddingBottom:"0px"}}>
+                    <div className="d-flex justify-content-between flex-column px-3">
+                        <div className={"btn-history-filter"} onClick={() => handleFilterChange('today')}>Today</div>
+                        <div className={"btn-history-filter"} onClick={() => handleFilterChange('yesterday')}>Yesterday</div>
+                        <div className={"btn-history-filter"} onClick={() => handleFilterChange('week')}>Week</div>
+                        <div className={"btn-history-filter"} onClick={() => handleFilterChange('month')}>Month</div>
+                    </div>
+                </Modal.Body>
             </Modal>
 
-            {/* Filter buttons */}
 
-                <div className="d-flex justify-content-between flex-column px-3">
-                    <div className={"btn-history-filter"} onClick={() => handleFilterChange('today')}>Today</div>
-                    <div className={"btn-history-filter"} onClick={() => handleFilterChange('yesterday')}>Yesterday</div>
-                    <div className={"btn-history-filter"} onClick={() => handleFilterChange('week')}>Week</div>
-                    <div className={"btn-history-filter"} onClick={() => handleFilterChange('month')}>Month</div>
-                </div>
 
 
             {/* Game list */}
-            <div>{filterGames()}</div>
+            {/*<div>{filterGames()}</div>*/}
         </>
     );
 };

97fddea3c2160cec98d8c44ecb0c36ef14d981d3
TychaK <kelvinkibugi@gmail.com>
2023-06-23 13:27:17 +0300

    Adding market group filters on more markets

--- src/components/all-markets.js
+++ src/components/all-markets.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useContext, useLayoutEffect, useState} from 'react';
+import React, {useCallback, useContext, useEffect, useLayoutEffect, useState} from 'react';
 import './test.css'
 import {useParams} from "react-router-dom";
 import {Context} from "../context/store";
@@ -13,116 +13,117 @@ const Header = React.lazy(() => import('./header/header'));
 const Footer = React.lazy(() => import('./footer/footer'));
 const Right = React.lazy(() => import('./right'));
 const SideBar = React.lazy(() => import('./sidebar/awesome/Sidebar'))
-const  AllMarkets= React.memo(
+const AllMarkets = React.memo(
     (props) => {
-    const [producerDown, setProducerDown] = useState(false);
-    const [allMarkets,setAllMarkets]=useState(true)
-    const params=useParams()
-    let url = new URL(window.location);
-    const {live} = props
-    const id = params.id
+        const [producerDown, setProducerDown] = useState(false);
+        const [allMarkets, setAllMarkets] = useState(true)
+        const params = useParams()
+        let url = new URL(window.location);
+        const {live} = props
+        const id = params.id
 
-    // const [userSlipsValidation, setUserSlipsValidation] = useState();
-    const [state, dispatch] = useContext(Context);
+        // const [userSlipsValidation, setUserSlipsValidation] = useState();
+        const [state, dispatch] = useContext(Context);
 
-    const [isLoading, setIsLoading] = useState(false);
+        const [isLoading, setIsLoading] = useState(false);
 
-    const findPostableSlip = () => {
-        let betslips = getBetslip() || {};
-        var values = Object.keys(betslips).map(function (key) {
-            return betslips[key];
-        });
-        return values;
-    };
-
-    useInterval(
-        () => {
-            let endpoint = live
-                ? "/v1/matches/live?id=" + id
-                : "/v1/matches?id=" + id;
-
-            let betslip = findPostableSlip();
-            let method = betslip ? "POST" : "GET";
-
-            makeRequest({ url: endpoint, method: method, data: betslip }).then(
-                ([_status, response]) => {
-                    dispatch({type: "SET", key: "all_markets", payload: response?.data||response});
-                    // setMatchWithMarkets(response?.data || response);
-                    if (response?.slip_data) {
-                        dispatch({type: "SET", key: "user_slip_validation", payload: response?.slip_data});
-                        // setUserSlipsValidation(response?.slip_data);
-                    }
-                    setProducerDown(response?.producer_status === 1);
-                }
-            );
-        },
-        live ? 5000 : null
-    );
-
-    const fetchPagedData = useCallback(async () => {
-        if (!isLoading && !isNaN(+id)) {
-            setIsLoading(true);
-            let betslip = findPostableSlip();
-            let endpoint = live
-                ? "/v1/matches/live?id=" + id
-                : "/v1/matches?id=" + id;
-
-            await makeRequest({ url: endpoint, method: "POST", data: betslip }).then(
-                ([status, result]) => {
-                    dispatch({type: "SET", key: "all_markets", payload: result?.data||result});
-                    // setMatchWithMarkets(result?.data || result);
-                    setProducerDown(result?.producer_status === 1);
-                    setIsLoading(false);
-                }
-            );
-        }
-    }, []);
-
-    useLayoutEffect(() => {
-        const abortController = new AbortController();
-        fetchPagedData();
-        return () => {
-            abortController.abort();
+        const findPostableSlip = () => {
+            let betslips = getBetslip() || {};
+            var values = Object.keys(betslips).map(function (key) {
+                return betslips[key];
+            });
+            return values;
         };
-    }, []);
-    const urlPath=window.location.pathname
-    const showDownload=(!urlPath.includes("nare-games")&&!urlPath.includes("gameplay")&&!urlPath.includes("smart-play")&&!urlPath.includes("betslip-slip")&&!urlPath.includes("nare-league")&&!urlPath.includes("bet-history")&&!urlPath.includes("standings")&&!urlPath.includes("results")&&!urlPath.includes("casino")&&!urlPath.includes("jackpot")&&!urlPath.includes("smart-soft")&&!urlPath.includes("virtuals")&&!urlPath.includes("match")&&!urlPath.includes("competition"))
 
+        useInterval(
+            () => {
+                let endpoint = live
+                    ? "/v1/matches/live?id=" + id
+                    : "/v1/matches?id=" + id;
 
-    return (
-        <div className={'flex-item'}>
-            <div className="item4"><Header/></div>
-            <div className={`flex-container ${!showDownload&&'top-spacing-page-no-download'}`}>
-                <div className="item1">
-                    {window.location.pathname.includes('/match/live')?<LiveSideBar/>:<SideBar loadCompetitions/>}
-                </div>
-                <div className="item2 size-all-markets" >
-                    <div className="gz home" style={{ width: "100%" ,marginBottom:"5rem"}}>
+                let betslip = findPostableSlip();
+                let method = betslip ? "POST" : "GET";
 
-                        <div className="homepage mobile-full-height">
+                makeRequest({url: endpoint, method: method, data: betslip}).then(
+                    ([_status, response]) => {
+                        dispatch({type: "SET", key: "all_markets", payload: response?.data || response});
+                        // setMatchWithMarkets(response?.data || response);
+                        if (response?.slip_data) {
+                            dispatch({type: "SET", key: "user_slip_validation", payload: response?.slip_data});
+                            // setUserSlipsValidation(response?.slip_data);
+                        }
+                        setProducerDown(response?.producer_status === 1);
+                    }
+                );
+            },
+            live ? 5000 : null
+        );
 
-                            <MarketList
-                                allMarkets={allMarkets}
-                                live={live}
-                                matchwithmarkets={state?.all_markets}
-                                pdown={producerDown}
-                            />
+        const fetchPagedData = useCallback(async () => {
+            if (!isLoading && !isNaN(+id)) {
+                setIsLoading(true);
+                let betslip = findPostableSlip();
+                let endpoint = live
+                    ? "/v1/matches/live?id=" + id
+                    : "/v1/matches?id=" + id;
 
+                await makeRequest({url: endpoint, method: "POST", data: betslip}).then(
+                    ([status, result]) => {
+                        dispatch({type: "SET", key: "all_markets", payload: result?.data || result});
+                        // setMatchWithMarkets(result?.data || result);
+                        setProducerDown(result?.producer_status === 1);
+                        setIsLoading(false);
+                    }
+                );
+            }
+        }, []);
+
+        useLayoutEffect(() => {
+            const abortController = new AbortController();
+            fetchPagedData();
+            return () => {
+                abortController.abort();
+            };
+        }, []);
+        const urlPath = window.location.pathname
+        const showDownload = (!urlPath.includes("nare-games") && !urlPath.includes("gameplay") && !urlPath.includes("smart-play") && !urlPath.includes("betslip-slip") && !urlPath.includes("nare-league") && !urlPath.includes("bet-history") && !urlPath.includes("standings") && !urlPath.includes("results") && !urlPath.includes("casino") && !urlPath.includes("jackpot") && !urlPath.includes("smart-soft") && !urlPath.includes("virtuals") && !urlPath.includes("match") && !urlPath.includes("competition"))
+        return (
+            <div className={'flex-item'}>
+                <div className="item4"><Header/></div>
+                <div className={`flex-container ${!showDownload && 'top-spacing-page-no-download'}`}>
+                    <div className="item1">
+                        {window.location.pathname.includes('/match/live') ? <LiveSideBar/> :
+                            <SideBar loadCompetitions/>}
+                    </div>
+                    <div className="item2 size-all-markets">
+                        <div className="gz home" style={{width: "100%", marginBottom: "5rem"}}>
+
+                            <div className="homepage mobile-full-height">
+
+                                <MarketList
+                                    allMarkets={allMarkets}
+                                    live={live}
+                                    matchwithmarkets={state?.all_markets}
+                                    pdown={producerDown}
+                                    groups={state?.market_groups}
+                                />
+
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="item3">
-                    <Right betslipValidationData={state?.user_slip_validation} test={true}  />
-                </div>
+                    <div className="item3">
+                        <Right betslipValidationData={state?.user_slip_validation} test={true}/>
+                    </div>
 
+                </div>
+                <div className="item6">
+                    <div className={"footer-mobile-none"}>
+                        <Footer/>
+                    </div>
+                </div>
             </div>
-            <div className="item6">
-                <div className={"footer-mobile-none"}>
-                <Footer/>
-            </div></div>
-        </div>
 
-    );
-});
+        );
+    });
 
 export default React.memo(AllMarkets);

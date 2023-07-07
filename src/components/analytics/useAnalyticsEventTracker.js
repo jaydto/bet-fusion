fe5a7947ec9a10c92f150f063a5c2ee274a83048
jaydto <johnckaris@gmail.com>
2023-07-07 07:49:36 +0300

    updated event handler for google and favebook

--- src/components/analytics/useAnalyticsEventTracker.js
+++ src/components/analytics/useAnalyticsEventTracker.js
@@ -1,10 +1,38 @@
 import React from "react";
 import ReactGA from "react-ga4";
+import ReactPixel from 'react-facebook-pixel';
 
-const useAnalyticsEventTracker = (category="Home") => {
-    const eventTracker = (action = "Page Visit", label = category) => {
-        ReactGA.event({category, action, label});
-    }
-    return eventTracker;
+export function trackEventToGTM(category, action, label) {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'customEvent',
+        eventCategory: category,
+        eventAction: action,
+        eventLabel: label
+    });
+
+    // Push the GTM event
+    window.dataLayer.push({
+        'event': 'gtmEvent',
+        'gtmEventType': 'gaEvent',
+        'gaEventCategory': category,
+        'gaEventAction': action,
+        'gaEventLabel': label
+    });
 }
+const useAnalyticsEventTracker = (category = 'Home', action = 'Page Visit') => {
+    const eventTracker = (action_data, label) => {
+        const gaCategory = action_data || action;
+        const gaLabel = label || category;
+
+        trackEventToGTM(category, gaCategory, gaLabel);
+        ReactGA.event({ category, action: gaCategory, label: gaLabel });
+        ReactPixel.track('CustomEvent', {
+            category: gaCategory,
+            action: gaLabel
+        });
+    };
+    return eventTracker;
+};
+
 export default useAnalyticsEventTracker;

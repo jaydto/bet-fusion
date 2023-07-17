508c619b8cd8c8f75b54e15dcb59d199f4b118ad
jaydto <johnckaris@gmail.com>
2023-07-17 11:16:15 +0300

    added and fixed jackpot page

--- src/components/Jackpot.js
+++ src/components/Jackpot.js
@@ -192,7 +192,7 @@ const Jackpot = React.memo(
             setActiveTab(eventKey);
         };
         return (
-            <div className={'flex-item jackpot'}>
+            <div className={'flex-item jackpot-container'}>
                 <div className="item4">
                     <Header jackpot={true}/>
                     <ToastContainer/>

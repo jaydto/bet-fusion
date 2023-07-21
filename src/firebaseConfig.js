39c8885d4bf5df0c44fabe9182c40ba5eda9e44d
jaydto <johnckaris@gmail.com>
2023-07-21 09:09:21 +0300

    firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,7 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import {initializeApp} from 'firebase/app';
+import {getAnalytics} from 'firebase/analytics';
+import {getAuth} from "firebase/auth"
+import {getMessaging} from "firebase/messaging";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBMscvFCSUG7AvfvmmYilyrJBoxk8VoezU",
@@ -7,12 +9,23 @@ const firebaseConfig = {
     projectId: "betnare",
     storageBucket: "betnare.appspot.com",
     messagingSenderId: "43489980882",
-    appId: "1:43489980882:web:eb216065b8ece00fe3580b",
-    measurementId: "G-SKE7NYZYYB"
+    appId: "1:43489980882:web:de63763c66f9aeafe3580b",
+    measurementId: "G-MMYJQ4544J"
 };
+let app;
+let analytics;
+let messaging;
+let auth;
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+try {
+    app = initializeApp(firebaseConfig);
+    analytics = getAnalytics(app);
+    messaging = getMessaging(app);
+    auth = getAuth(app)
 
-export { app, analytics };
+} catch (error) {
+    console.error("Error initializing Firebase:", error);
+}
+
+export {auth, app, analytics, messaging};
 

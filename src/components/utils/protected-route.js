f165b6ca45216c1727006550328abdaf60628ff2
jaydto <johnckaris@gmail.com>
2023-07-27 13:52:34 +0300

    added tracking for links clicked that have a protected route

--- src/components/utils/protected-route.js
+++ src/components/utils/protected-route.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Navigate} from "react-router-dom";
-import {getFromLocalStorage} from './local-storage';
+import {getFromLocalStorage, setLocalStorage} from './local-storage';
 import 'react-toastify/dist/ReactToastify.css';
 
 const ProtectedRoute = React.memo(
     ({children}) => {
 
     const user = getFromLocalStorage("user");
+    const path=window.location.pathname;
+    setLocalStorage("ActiveLink", path)
     return user?.token ? children : <Navigate to="/login" />;
 })
 

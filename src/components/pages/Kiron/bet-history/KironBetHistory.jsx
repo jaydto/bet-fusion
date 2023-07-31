937e2162acdeb3b6d014d6eb70f67e4458d1116c
jaydto <johnckaris@gmail.com>
2023-07-31 14:58:56 +0300

    added logic for kiron bethistory

--- src/components/pages/Kiron/bet-history/KironBetHistory.jsx
+++ src/components/pages/Kiron/bet-history/KironBetHistory.jsx
@@ -8,10 +8,15 @@ import './kiron.css';
 import {Link, useNavigate, useParams} from 'react-router-dom';
 
 import {useDispatch, useSelector} from 'react-redux'; // Import useDispatch hook
-import {nareLeagueBetHistory, nareLeagueOldBets} from '../../../../redux/nareLeague';
+import {
+    nareLeagueBetDetails,
+    nareLeagueBetHistory,
+    nareLeagueOldBetDetails,
+    nareLeagueOldBets, resetState
+} from '../../../../redux/nareLeague';
 
-const OldBetDetails=React.lazy(()=>import('./OldBetDetails'))
-const ActiveBetDetails=React.lazy(()=>import('./ActiveBetDetails'))
+const OldBetDetails = React.lazy(() => import('./OldBetDetails'))
+const ActiveBetDetails = React.lazy(() => import('./ActiveBetDetails'))
 
 const Styles = {
     contain: {
@@ -37,7 +42,7 @@ const KironBetHistory = React.memo(
         const dispatchRedux = useDispatch()
         const navigate = useNavigate();
         const {betID} = useParams()
-        console.log("betID", betID)
+
         const [activeTab, setActiveTab] = useState(getFromLocalStorage("tab_history_kiron") || "active");
         const loading = useSelector((state) => state.nareLeague.loading)
         //functions to fetch active and old bets from the API
@@ -45,113 +50,124 @@ const KironBetHistory = React.memo(
             dispatchRedux(nareLeagueBetHistory())
         }, []);
 
+        const fetchOldBetDetails = useCallback(async () => {
+            const data = {
+                'bet_id': betID
+            };
+            await dispatchRedux(nareLeagueOldBetDetails(data))
+        }, [betID]);
+
         const fetchOldBets = useCallback(async () => {
-           dispatchRedux(nareLeagueOldBets())
+            dispatchRedux(nareLeagueOldBets())
         }, []);
+
+        const fetchActiveBetDetails = useCallback(async () => {
+            // if (isLoading) return;
+
+            const betIdData = {
+                bet_id: betID,
+            };
+            dispatchRedux(nareLeagueBetDetails(betIdData))
+
+        },[]);
 
         //function to fetch old bet details
         useEffect(() => {
             if (betID) {
-                return
-            } else {
-                if (activeTab == "active") {
+                // If there's a betID, it means we're viewing the details of a specific bet,
+                if (activeTab == "active")
+                {
+                    fetchActiveBetDetails()
+                } else {
+                    fetchOldBetDetails()
+                }
+            }else{
+                // When changing the active tab, fetch the data accordingly
+                if (activeTab == "active")
+                {
                     fetchData();
-                    const payload = {
-                        start: '', round: '', end: ''
-                    }
-                    dispatch({type: "SET", key: 'current_selection_period', payload: payload})
+                   dispatchRedux(resetState('current_selection_period'))
                 } else {
                     fetchOldBets();
-                    const payload = {
-                        start: '', round: '', end: ''
-                    }
-                    dispatch({type: "SET", key: 'current_selection_period', payload: payload})
+                    dispatchRedux(resetState('current_selection_period'))
                 }
             }
 
 
         }, [activeTab, betID]);
 
-		const activeBetHistory=useSelector((state)=>state.nareLeague.bet_history_data)
-		const oldBetHistory=useSelector((state)=>state.nareLeague.old_bets_data)
+        const activeBetHistory = useSelector((state) => state.nareLeague.bet_history_data)
+        const oldBetHistory = useSelector((state) => state.nareLeague.old_bets_data)
 
-        const TabbedDetails = React.memo(
+        const OldBets = React.memo(
             () => {
-            const handleTabSelect = (eventKey) => {
-                setActiveTab(eventKey); // Update activeTab state when a tab is selected
-                setLocalStorage("tab_history_kiron", eventKey)
-                navigate('/bet-history/')
-            };
-            const BetArchive = React.memo(
-                () => {
                 const {betID} = useParams()
-                const swap = () => {
-                    setActiveTab('older')
-                    setLocalStorage("tab_history_kiron", 'older')
+                const handleOldBetClick = (betID) => {
+                    // Navigate to the old bet details page when clicked
+                    navigate(`/bet-history/${betID}`);
+                };
 
-                }
-                const OldBets = () => {
-                    return (
-                        <>  {
-                            oldBetHistory && oldBetHistory?.map((bets,index) => (
-                                <Link to={`/bet-history/${bets?.bet_id}`} onClick={swap} key={index}>
-                                    <div key={bets?.bet_id} className="bet_item">
-                                        <>
-                                            <div className="left mt-0">
-                                                <p><span className="bold">Bet Id</span> : {bets?.bet_id}</p>
-                                                <p><span className="bold">Date</span> : {bets?.created}</p>
-                                            </div>
-                                            <div className=" mt-0 kiron-right">
-                                                <p><span className="bold">Bet Amount</span> : {bets?.bet_amount}
-                                                    <span style={{position: "relative"}}>
+                return (
+                    <>  {
+                        oldBetHistory && oldBetHistory?.map((bets, index) => (
+                            <div onClick={() => handleOldBetClick(bets?.bet_id)}  key={index}>
+                                <div key={bets?.bet_id} className="bet_item">
+                                    <>
+                                        <div className="left mt-0">
+                                            <p><span className="bold">Bet Id</span> : {bets?.bet_id}</p>
+                                            <p><span className="bold">Date</span> : {bets?.created}</p>
+                                        </div>
+                                        <div className=" mt-0 kiron-right">
+                                            <p><span className="bold">Bet Amount</span> : {bets?.bet_amount}
+                                                <span style={{position: "relative"}}>
 
                                                     </span>
-                                                </p>
-                                                <p className="ban  ban-value-data mt-1"
+                                            </p>
+                                            <p className="ban  ban-value-data mt-1"
+                                               style={{
+                                                   backgroundColor: parseInt(bets?.status) === 5 ? 'hsl(120, 70%, 50%)' : parseInt(bets?.status) === 3 ? '#ff9900' : 'inherit',
+                                                   borderRadius: '20px',
+                                                   height: "1.8em",
+                                                   marginTop: "-10px",
+                                                   width: '1.6cm',
+                                                   textAlign: 'center',
+                                                   fontSize: "small",
 
+                                               }}
 
-                                                   style={{
-                                                       backgroundColor: parseInt(bets?.status) === 5 ? 'hsl(120, 70%, 50%)' : parseInt(bets?.status) === 3 ? '#ff9900' : 'inherit',
-                                                       borderRadius: '20px',
-                                                       height: "1.8em",
-                                                       marginTop: "-10px",
-                                                       width: '1.6cm',
-                                                       textAlign: 'center',
-                                                       fontSize: "small",
+                                            >
+                                                {parseInt(bets?.status) === 1
+                                                    ? 'placed'
+                                                    : parseInt(bets?.status) === 3
+                                                        ? 'Not won'
+                                                        : parseInt(bets?.status) === 24
+                                                            ? 'Cancelled'
+                                                            : parseInt(bets?.status) === 9
+                                                                ? 'Jackpot'
+                                                                : parseInt(bets?.status) === 25
+                                                                    ? 'Voided'
+                                                                    : parseInt(bets?.status) === 5
+                                                                        ? 'Won'
+                                                                        : 'Unknown Status'}
+                                            </p>
+                                        </div>
+                                    </>
+                                </div>
 
-                                                   }}
+                            </div>
 
-                                                >
-                                                    {parseInt(bets?.status) === 1
-                                                        ? 'placed'
-                                                        : parseInt(bets?.status) === 3
-                                                            ? 'Not won'
-                                                            : parseInt(bets?.status) === 24
-                                                                ? 'Cancelled'
-                                                                : parseInt(bets?.status) === 9
-                                                                    ? 'Jackpot'
-                                                                    : parseInt(bets?.status) === 25
-                                                                        ? 'Voided'
-                                                                        : parseInt(bets?.status) === 5
-                                                                            ? 'Won'
-                                                                            : 'Unknown Status'}
-                                                </p>
-                                            </div>
-                                        </>
-                                    </div>
+                        ))
+                    }
+                    </>
+                )
+            })
 
-                                </Link>
-
-                            ))
-                        }
-                        </>
-                    )
-                }
-
+        const BetArchive = React.memo(
+            () => {
                 return (
                     <div className="contain">
                         {betID ? (
-                            <OldBetDetails betID={betID}/>
+                            <OldBetDetails />
                         ) : (
                             <OldBets/>
                         )}
@@ -159,8 +175,8 @@ const KironBetHistory = React.memo(
                 );
             });
 
-            const ActiveBets = React.memo(
-                () => {
+        const ActiveBets = React.memo(
+            () => {
                 const {betID} = useParams()
                 const swap = () => {
                     setActiveTab('active')
@@ -169,7 +185,7 @@ const KironBetHistory = React.memo(
                 const ActiveBet = () => {
                     return (
                         <>  {
-							activeBetHistory && activeBetHistory?.map((bets, index) => (
+                            activeBetHistory && activeBetHistory?.map((bets, index) => (
                                 <Link to={`/bet-history/${bets?.bet_id}`} onClick={swap} key={index}>
                                     <div key={bets?.bet_id} className="bet_item">
                                         <>
@@ -178,8 +194,9 @@ const KironBetHistory = React.memo(
                                                 <p><span className="bold">Date</span> : {bets?.bet_date}</p>
                                             </div>
                                             <div className=" kiron-right mt-0">
-                                                <p><span className="bold">Bet Amount </span>: {bets?.bet_amount} <span
-                                                    style={{position: "relative"}}>
+                                                <p><span className="bold">Bet Amount </span>: {bets?.bet_amount}
+                                                    <span
+                                                        style={{position: "relative"}}>
                                                     </span></p>
                                                 <p className="ban ban-value-data mt-1"
                                                    style={{
@@ -218,7 +235,7 @@ const KironBetHistory = React.memo(
                 return (
                     <div className="contain">
                         {betID ? (
-                            <ActiveBetDetails betID={betID} />
+                            <ActiveBetDetails betID={betID}/>
                         ) : (
                             <ActiveBet/>
                         )}
@@ -226,32 +243,40 @@ const KironBetHistory = React.memo(
                 );
             });
 
-            return (
-                <Tabs
-                    variant={'tabs'}
-                    defaultActiveKey={activeTab || getFromLocalStorage("tab_history_kiron")}
-                    id=""
-                    className="background-primary kiron-bet-history-tabs"
-                    justify
-                    onSelect={handleTabSelect}>
-                    <Tab eventKey="active" title="ACTIVE BETS" className={'background-primary'}>
-                        {loading ?
-                            <div className={`text-center mt-2 text-white d-block`}>
-                                <Spinner animation={'grow'} size={'lg'}/>
-                            </div> :
-                            <ActiveBets/>}
-                    </Tab>
-                    <Tab eventKey="older" title="OLDER BETS" className={'background-primary'}>
-                        {loading ?
-                            <div className={`text-center mt-2 text-white d-block`}>
-                                <Spinner animation={'grow'} size={'lg'}/>
-                            </div>
-                            : <BetArchive/>
-                        }
-                    </Tab>
-                </Tabs>
-            )
-        })
+        const TabbedDetails = React.memo(
+            () => {
+                const handleTabSelect = (eventKey) => {
+                    setActiveTab(eventKey); // Update activeTab state when a tab is selected
+                    setLocalStorage("tab_history_kiron", eventKey)
+                    navigate('/bet-history/')
+                };
+
+                return (
+                    <Tabs
+                        variant={'tabs'}
+                        defaultActiveKey={activeTab || getFromLocalStorage("tab_history_kiron")}
+                        id=""
+                        className="background-primary kiron-bet-history-tabs"
+                        justify
+                        onSelect={handleTabSelect}>
+                        <Tab eventKey="active" title="ACTIVE BETS" className={'background-primary'}>
+                            {loading ?
+                                <div className={`text-center mt-2 text-white d-block`}>
+                                    <Spinner animation={'grow'} size={'lg'}/>
+                                </div> :
+                                <ActiveBets/>}
+                        </Tab>
+                        <Tab eventKey="older" title="OLDER BETS" className={'background-primary'}>
+                            {loading ?
+                                <div className={`text-center mt-2 text-white d-block`}>
+                                    <Spinner animation={'grow'} size={'lg'}/>
+                                </div>
+                                : <BetArchive/>
+                            }
+                        </Tab>
+                    </Tabs>
+                )
+            })
 
         return (
             <div className="d-flex flex-row justify-content-between">

0d904828f20c32451edd3a4be82971b71d46327d
jaydto <johnckaris@gmail.com>
2023-08-02 21:50:36 +0300

    top header navigations styling

--- src/components/utils/fetch-request.js
+++ src/components/utils/fetch-request.js
@@ -4,8 +4,8 @@ import { getFromLocalStorage, setLocalStorage } from "./local-storage";
 
 const ENC_KEY = '2bdVweTeI42s5mkLdYHyklTMxQS5gLA7MDS6FA9cs1uobDXeruACDic0YSU3si04JGZe4Y';
 // export const BASE_URL = 'http://localhost:5000';
-// export const BASE_URL = 'https://api.betnare.com';
-export const BASE_URL = 'https://testapi.betnare.co.ke';
+export const BASE_URL = 'https://api.betnare.com';
+// export const BASE_URL = 'https://testapi.betnare.co.ke';
 
 const makeRequest = async ({ url, method, data = null, use_jwt = false }) => {
     url = BASE_URL + url;

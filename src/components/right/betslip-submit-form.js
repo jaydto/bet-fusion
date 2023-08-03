dde3436b25e46b5d07632a8ae1337ff5702650cf
jaydto <johnckaris@gmail.com>
2023-08-03 13:20:07 +0300

    updated taxes

--- src/components/right/betslip-submit-form.js
+++ src/components/right/betslip-submit-form.js
@@ -333,9 +333,9 @@ const BetslipSubmitForm = React.memo(
 
         const updateWinnings = useCallback(() => {
             if (betslip) {
-                let stake_after_tax = (Float(stake) / Float(107.5)) * 100;
+                let stake_after_tax = (Float(stake) / Float(112.5)) * 100;
                 let stake_after_tax_boosted =
-                    ((Float(stake) + Float(multiBoostAmount)) / Float(107.5)) * 100;
+                    ((Float(stake) + Float(multiBoostAmount)) / Float(112.5)) * 100;
 
                 let ext = Float(stake) - Float(stake_after_tax);
                 let ext_boosted =

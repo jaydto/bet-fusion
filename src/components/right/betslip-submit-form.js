ce5ddacd69128577bd0ed7dabcfa01eabe565413
jaydto <johnckaris@gmail.com>
2023-08-15 11:54:23 +0300

    increased the timeout to show the message time

--- src/components/right/betslip-submit-form.js
+++ src/components/right/betslip-submit-form.js
@@ -123,7 +123,7 @@ const BetslipSubmitForm = React.memo(
                     dispatch({type: "SET", key: "user", payload: u});
                     setTimeout(() => {
                         setMessage(null)
-                    }, 6000)
+                    }, 8000)
                     dispatch({type: "SET", key: "placebet", payload: true});
                 }
             });

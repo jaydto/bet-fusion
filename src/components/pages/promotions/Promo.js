293639e82f3cc4c096ef6b60b1ed260940b3131a
jaydto <johnckaris@gmail.com>
2023-08-18 19:20:21 +0300

    betnare-web promotion header top

--- src/components/pages/promotions/Promo.js
+++ src/components/pages/promotions/Promo.js
@@ -81,7 +81,7 @@ const Promo = () => {
                                              }}/>
                                             </span>
                                     <div
-                                        className="shadow-sm d-flex justify-content-center col-10 p-2 shadow-sm promotion-header text-white">
+                                        className="shadow-sm d-flex justify-content-center col-8 p-2 shadow-sm promotion-header text-white">
                                         BETNARE PROMOTIONS
                                     </div>
                                 </div>

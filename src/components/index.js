9e0db2202be8e5469d7442ffdaa09259d67e62af
jaydto <johnckaris@gmail.com>
2023-08-22 08:01:47 +0300

    loading indicator on filter market options

--- src/components/index.js
+++ src/components/index.js
@@ -250,7 +250,7 @@ const Index = React.memo(
 
         const fetchData = useCallback(async () => {
             // setFetching(true)
-            // setLoading(true)
+            setLoading(true)
             let tab = location.pathname.replace("/", "") || 'highlights';
             let tabInfo = window.location.pathname
             tabInfo = tabInfo.substring(tabInfo.lastIndexOf('/') + 1)

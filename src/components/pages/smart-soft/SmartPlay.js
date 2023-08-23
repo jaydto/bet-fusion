9b3ef7005ccd14264d75fc52d4423b877803c7e5
jaydto <johnckaris@gmail.com>
2023-08-23 17:42:27 +0300

    fullscreeen on smartplay

--- src/components/pages/smart-soft/SmartPlay.js
+++ src/components/pages/smart-soft/SmartPlay.js
@@ -83,8 +83,8 @@ const SmartPlay = React.memo(
 
         // // Function to update the iframe height
         const updateIframeHeight = useCallback(() => {
-            console.log("this was called to resize", maxIframeHeight)
-            setIframeHeight(isCustomFullscreen ? 800 : 700); // Set the fixed height here
+            // console.log("this was called to resize", maxIframeHeight)
+            setIframeHeight(isCustomFullscreen ? 660 : 800); // Set the fixed height here
         }, []);
 
 
@@ -101,47 +101,70 @@ const SmartPlay = React.memo(
             };
         }, [updateIframeHeight, isCustomFullscreen]);
 
+        useEffect(() => {
+            console.log("called escape here entry to function")
+            const handleEsc = (event) => {
+                if (event.key === 'Escape') {
+                    console.log("called escape here")
+                    setCustomFullscreen(false);
+                }
+            };
+            window.addEventListener('keydown', handleEsc);
+
+            return () => {
+                window.removeEventListener('keydown', handleEsc);
+            };
+        }, []);
+
+
+
         const toggleFullscreen = () => {
-            // const element = document.documentElement; // Fullscreen the whole document
+            const element = document.documentElement; // Fullscreen the whole document
+            // console.log("Element fullscreen is now ... ",element)
 
             if (!isCustomFullscreen) {
-                // try {
-                //     if (element?.requestFullscreen) {
-                //         element?.requestFullscreen();
-                //     } else if (element?.mozRequestFullScreen) {
-                //         element?.mozRequestFullScreen();
-                //     } else if (element?.webkitRequestFullscreen) {
-                //         element?.webkitRequestFullscreen();
-                //     } else if (element?.msRequestFullscreen) {
-                //         element?.msRequestFullscreen();
-                //     }
-                // } catch (err) {
-                //     //there was an error encountered
-                //     console.error("error_message", err)
-                // }
-
+                try {
+                    if (element?.requestFullscreen) {
+                        element?.requestFullscreen();
+                    } else if (element?.mozRequestFullScreen) {
+                        element?.mozRequestFullScreen();
+                    } else if (element?.webkitRequestFullscreen) {
+                        element?.webkitRequestFullscreen();
+                    } else if (element?.msRequestFullscreen) {
+                        element?.msRequestFullscreen();
+                    }
+                } catch (err) {
+                    //there was an error encountered
+                    console.error("error_message", err)
+                }
 
                 setCustomFullscreen(true);
 
             } else {
-                // try {
-                //     if (document.exitFullscreen) {
-                //         document.exitFullscreen();
-                //     } else if (document.mozCancelFullScreen) {
-                //         document.mozCancelFullScreen();
-                //     } else if (document.webkitExitFullscreen) {
-                //         document.webkitExitFullscreen();
-                //     } else if (document.msExitFullscreen) {
-                //         document.msExitFullscreen();
-                //     }
-                // } catch (err) {
-                //     console.error("error_encountered", err)
-                // }
+                try {
+                    if(!document.fullscreenEnabled){
+                        setCustomFullscreen(false);
+                    }
+                    if(document.fullscreenElement){
+
+                        if (document.exitFullscreen) {
+                            document.exitFullscreen();
+                        } else if (document.mozCancelFullScreen) {
+                            document.mozCancelFullScreen();
+                        } else if (document.webkitExitFullscreen) {
+                            document.webkitExitFullscreen();
+                        } else if (document.msExitFullscreen) {
+                            document.msExitFullscreen();
+                        }
+                    }
+
+                } catch (err) {
+                    console.error("error_encountered", err)
+                }
 
                 setCustomFullscreen(false);
             }
         };
-
         const configureDemoGame = () => {
             setGameUrl(`https://www.smartsoftgaming.com/GameDemo/${game || 'JetX'}?currency=USD&lang=EN&return_url=https://betnare.com`)
             setGameUrlLoaded(true)

e300bb4f112e2c4b9c76d98e75cc634b1a730765
jaydto <johnckaris@gmail.com>
2023-09-01 17:50:56 +0300

    updated protected route to only check in local storage

--- src/components/utils/protected-route.js
+++ src/components/utils/protected-route.js
@@ -1,20 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {Navigate} from "react-router-dom";
 import {getFromLocalStorage, setLocalStorage} from './local-storage';
 import 'react-toastify/dist/ReactToastify.css';
-import {useSelector} from "react-redux";
+
 
 const ProtectedRoute = React.memo(
     ({children}) => {
 
-        const userData = useSelector((state) => state.data.user)
-        const [user, setUser] = useState(getFromLocalStorage("user"))
-
-        useEffect(() => {
-            if (userData) {
-                setUser(userData || getFromLocalStorage("user"))
-            }
-        }, userData)
+        const user = getFromLocalStorage("user")
 
         const path = window.location.pathname;
         setLocalStorage("ActiveLink", path)

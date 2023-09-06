70f229c116517080c86035ca2ec84b9c06be5d52
jaydto <johnckaris@gmail.com>
2023-09-06 21:06:18 +0300

    importing bootstrap in lite.css

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import store from "./redux/store";
 import {BrowserRouter, Navigate, Route, Routes, useNavigate,} from 'react-router-dom'
 import {setLocalStorage} from "./components/utils/local-storage";
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/application.css';
 import './assets/css/tolkits.css';
 import './assets/css/sidebar-menu.css';

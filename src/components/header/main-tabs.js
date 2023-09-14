c41cf6dd415db2c46b60fda00749f7ad0fb20e96
jaydto <johnckaris@gmail.com>
2023-09-14 17:44:16 +0300

    -checks on competition and normal matches tab

--- src/components/header/main-tabs.js
+++ src/components/header/main-tabs.js
@@ -2,26 +2,30 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const MainTabs = (props) => {
-    const {tab} = props;
+    const {tab, competition} = props;
 
     const u_class = tab === 'upcoming' ? "home-tabs-filters-active myButton" : "home-tabs-filters myButton";
     const c_class = tab === "countries" ? "home-tabs-filters-active myButton" : "home-tabs-filters myButton";
     const h_class = (!tab || tab === 'highlights') ? "home-tabs-filters-active myButton" : "home-tabs-filters myButton";
     const t_class = tab === 'tomorrow' ? "home-tabs-filters-active myButton" : "home-tabs-filters myButton";
 
-    const getLink = (tab) => {
+    const getLink = (tab_competition) => {
 
         const urlSearchParams = new URLSearchParams(window.location.search);
         const url = new URL(window.location)
         const params = Object.fromEntries(urlSearchParams.entries());
-        // console.log("pathname", params)
-        url.pathname = `/${tab}`
-        url.searchParams.set('sport_id', 79)
+        let sportId = new URLSearchParams(window.location.search).get('sport_id') || '79'
+        if(competition){
+            url.pathname = `/${competition}`
+        }else{
+            url.pathname = `/${tab}`
+        }
+        url.searchParams.set('sport_id', sportId)
         Object.keys(params)?.forEach((param, val) => {
             url.searchParams.set(param, params[param])
         })
 
-        return url.searchParams
+        return url.searchParams.toString()
 
     }
 
@@ -30,17 +34,17 @@ const MainTabs = (props) => {
         <div className={"full-mobile main-navigations"} >
             <div className=" d-flex flex-row ">
                 <div className="col  px-1  ">
-                    <Link className={`cursor-pointer w-100 ${h_class}`} to={'/highlights?' + getLink()}>
+                    <Link className={`cursor-pointer w-100 ${h_class}`} to={(competition?`/highlights-competition${competition}?`:'/highlights?') + getLink('highlights')}>
                         <span className="col ">Highlights</span>
                     </Link>
                 </div>
                 <div className="col  px-1  ">
-                    <Link className={`cursor-pointer w-100 ${u_class}`} to={'/upcoming?' + getLink()}>
+                    <Link className={`cursor-pointer w-100 ${u_class}`} to={(competition?`/upcoming-competition${competition}?`:'/upcoming?') + getLink('upcoming')}>
                         <span className="col ">Upcoming</span>
                     </Link>
                 </div>
                 <div className="col  px-1  ">
-                    <Link className={`cursor-pointer w-100 ${t_class}`} to={'/tomorrow?' + getLink()}>
+                    <Link className={`cursor-pointer w-100 ${t_class}`} to={(competition?`/tomorrow-competition${competition}?`:'/tomorrow?') + getLink('tomorrow')}>
                         <span className="col ">Tomorrow</span>
                     </Link>
                 </div>

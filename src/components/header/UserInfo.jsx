c666d869ff4f5fc679edb30d2976827229844c32
jaydto <johnckaris@gmail.com>
2023-09-25 18:54:07 +0300

    link color

--- src/components/header/UserInfo.jsx
+++ src/components/header/UserInfo.jsx
@@ -129,7 +129,7 @@ export const UserInfo = React.memo(
                             </Link>}
                         {pathname !== '/signup' && <div className='d-flex align-items-baseline'>
                             <div className={` align-items-center  ${state?.searching ? 'd-none' : 'd-flex'}`}>
-                                <div className="cursor-pointer"  title="Search"
+                                <div className="cursor-pointer link-color"  title="Search"
                                       onClick={() => {
                                           showSearchBar();
                                           gaEventTracker('Visit Search')

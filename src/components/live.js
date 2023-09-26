3e9353d891271816fe30a23495da85c9f534b6f6
jaydto <johnckaris@gmail.com>
2023-09-26 14:06:29 +0300

    -cleanup unused code -cleanup spa preparation for llive page

--- src/components/live.js
+++ src/components/live.js
@@ -17,22 +17,17 @@ import {useDispatch, useSelector} from "react-redux";
 import {setMatchBetslip} from "../redux/bettingSlice";
 import {MatchHeaderRow} from "./matches";
 import {getFromLocalStorage} from "./utils/local-storage";
-import {removeScrollPosition, setScrollPast, setScrollPosition, setScrollToTop} from "../redux/ScrollBehavior";
 
-const Footer = React.lazy(() => import('./footer/footer'));
-const CarouselLoader = React.lazy(() => import('./carousel'));
 const MatchList = React.lazy(() => import('./matches'));
-const Right = React.lazy(() => import('./right'));
 
 const Live = React.memo(
     () => {
-        const {height, width} = useWindowDimensions();
+        const { width} = useWindowDimensions();
         const {spid} = useParams();
         const [sportID, setSportID] = useState(79)
 
         const dispatchRedux=useDispatch()
         const producer_down=useSelector((state)=>state.matchesData.live_producer_down)
-        const user_slip_validation=useSelector((state)=>state.matchesData.live_user_slip_validation)
         const fetching=useSelector((state)=>state.matchesData.live_fetching)
         const liveMatches=useSelector((state)=>state.matchesData.live_matches)
         const [matches, setMatches]=useState()
@@ -88,7 +83,7 @@ const Live = React.memo(
             return ()=>{
                 dispatchRedux(stopFetchingMatches())}
         },[sportID])
-
+        // todo check impact of this
         useEffect(() => {
             const new_sport_id = spid
             if (sportID !== new_sport_id) {
@@ -96,80 +91,20 @@ const Live = React.memo(
             }
         })
 
-        const homePageRef = useRef()
-        const scrolledPast=useSelector((state)=>state.scroll.scroll_past)
-        const scrolledToTop=useSelector((state)=>state.scroll.scroll_top)
-
-
-        useEffect(() => {
-            const handleScroll = () => {
-                if (homePageRef.current) {
-                    const scrollPosition = homePageRef.current.scrollTop;
-                    if (!scrolledPast && scrollPosition > 10) {
-                        dispatchRedux(setScrollPosition())
-                        dispatchRedux(setScrollPast({scroll_past:true}))
-                        dispatchRedux(setScrollToTop({scroll_top:false}))
-
-                    } else if (!scrolledToTop && scrollPosition <= 10) {
-                        dispatchRedux(removeScrollPosition())
-                        dispatchRedux(setScrollPast({scroll_past:false}))
-                        dispatchRedux(setScrollToTop({scroll_top:true}))
-
-                    }
-                }
-            };
-
-            if (homePageRef.current) {
-                homePageRef.current.addEventListener('scroll', handleScroll);
-            }
-
-            return () => {
-                if (homePageRef.current) {
-                    homePageRef.current.removeEventListener('scroll', handleScroll);
-                }
-            };
-        }, [homePageRef, scrolledPast, scrolledToTop]);
 
         const fetchAdditionalData=()=>{
-            console.log("fetching live")
+            return
         }
 
         return (
-            <div className={'flex-item'}>
-                <div className="item4">
-                    <ToastContainer/></div>
-                <div className="flex-container">
-                    <div className="item1 live">
-                        <div className={"mobile-live-remove"}>
-                            <LiveSideBar/>
-                        </div>
-                    </div>
-                    <div className="item2 live-top">
-                        <div className="gz home match-overflow ">
-                            <div className="homepage mobile-full-height" ref={homePageRef}
-                                 style={width < 991 ? {height: `${height}px`, overflowY: 'auto'} : {}}>
-                                <CarouselLoader/>
-                                <div className={`${width <= 991 ? "d-block" : "d-none"}`}>
-                                    <LiveSideBar spid={spid}/>
-                                </div>
-                                {matches&&<MatchHeaderRow live={true} first_match={matches ? matches[0] : {}} loading={fetching} spid={spid}/>}
-                                {fetching ? <SkeletonMobileLive/> : matches &&
-                                    <MatchList live={1}  fetching={true} matches={matches} pdown={producer_down} onEndReached={fetchAdditionalData}/>}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item3">
-                        <Right betslipValidationData={user_slip_validation} jackpotData={matches?.meta} test={true}
-                               live={true}/>
-                    </div>
-
-                </div>
-                <div className="item6">
-                    <div className={"footer-mobile-none"}>
-                        <Footer/>
-                    </div>
-                </div>
-            </div>
+           <>
+               <div className={`${width <= 991 ? "d-block" : "d-none"}`}>
+                   <LiveSideBar spid={spid}/>
+               </div>
+               {matches&&<MatchHeaderRow live={true} first_match={matches ? matches[0] : {}} loading={fetching} spid={spid}/>}
+               {fetching ? <SkeletonMobileLive/> : matches &&
+                   <MatchList live={1}  fetching={true} matches={matches} pdown={producer_down} onEndReached={fetchAdditionalData}/>}
+           </>
 
         );
     });

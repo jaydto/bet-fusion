7726713fac1b121e35bba32870cbc5b43893e1e7
jaydto <johnckaris@gmail.com>
2023-11-30 16:49:26 +0300

    fix tax calculation removed percentile information

--- src/components/right/betslip-submit-form.js
+++ src/components/right/betslip-submit-form.js
@@ -332,9 +332,9 @@ const BetslipSubmitForm = React.memo(
 
         const updateWinnings = useCallback(() => {
             if (betslip) {
-                let stake_after_tax = (Float(stake) / Float(112.5)) * 100;
+                let stake_after_tax = (Float(stake) / Float(112.5/100));
                 let stake_after_tax_boosted =
-                    ((Float(stake) + Float(multiBoostAmount)) / Float(112.5)) * 100;
+                    ((Float(stake) + Float(multiBoostAmount)) / Float(112.5/100));
 
                 let ext = Float(stake) - Float(stake_after_tax);
                 let ext_boosted =
@@ -775,7 +775,7 @@ const BetslipSubmitForm = React.memo(
                                 </tr>}
 
                                 <tr className="bet-win-tr hide-on-affix">
-                                    <td className={"bet-align-left tax-info"}> Excise Tax (12.5%)</td>
+                                    <td className={"bet-align-left tax-info"}> Excise Tax </td>
                                     <td className={"bet-align-right tax-info"}>KES. <span
                                         id="tax">{formatNumber(hasMultiBetBoost ? exciseTaxBoosted : exciseTax)}</span>
                                     </td>
@@ -784,7 +784,7 @@ const BetslipSubmitForm = React.memo(
                                     ''
                                 ) : (
                                     <tr className="bet-win-tr hide-on-affix">
-                                        <td className={"bet-align-left tax-info"}> Withholding (20%)</td>
+                                        <td className={"bet-align-left tax-info"}> Withholding </td>
                                         <td className={"bet-align-right tax-info"}>KES. <span
                                             id="tax">{formatNumber(hasMultiBetBoost ? withholdingTaxBoosted : withholdingTax)}</span>
                                         </td>

849f460c0c9ebceb0f5381ab8da5065ce0eb78c2
jaydto <johnckaris@gmail.com>
2024-01-04 11:38:25 +0300

    final test for promo

--- src/components/defaultPage.js
+++ src/components/defaultPage.js
@@ -234,6 +234,7 @@ const DefaultPage = React.memo(
         const user_slip_validation_live=useSelector((state)=>state.matchesData.live_user_slip_validation)
 
 
+    
         return (
             <div className={'flex-item'}>
                 <div className={bottom_sheet ? 'pointer-event-handler item4' : "item4"}>
@@ -244,7 +245,7 @@ const DefaultPage = React.memo(
                         {pathname.includes('live')?<LiveSideBar spid={spid}/>:<SideBar loadCompetitions/>}
                     </div>
                     <div className={bottom_sheet ? `pointer-event-handler item2` : `item2 ${pathname.includes('match')?' size-all-markets':pathname.includes('live')?' live-top':''}  ${
-                  close_call_to_action?pathname.includes("competition")?'':settings?.active_promotion?.app_promo?.promo_active === "0"? "top-position-mobile-view active":"top-position-mobile-view": pathname.includes("competition")?'':"top-position-mobile-view active"}`}
+                  close_call_to_action?pathname.includes("competition")?'':settings?.active_promotion?.app_promo?.promo_active === "0"? "top-position-mobile-view active":"top-position-mobile-view active": pathname.includes("competition")?'':settings?.active_promotion?.app_promo?.promo_active === "0"?"top-position-mobile-view active":"top-position-mobile-view"}`}
                          style={bottom_sheet ? {opacity: '0.5', background: '#13171c'} : {}}>
                         <div className={`gz home match-overflow ${competitionpath&&'competition-mobile-top'} `}>
                             <div className={`homepage mobile-full-height ${pathname.includes('match')?' all-markets':''}`} ref={homePageRef} style={width < 991 ? {height: `${height}px`, overflowY: 'auto'} : {}}>

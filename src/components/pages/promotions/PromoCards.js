c9e545f8e9a975bb314782007fd94ea6e1a3c544
jaydto <johnckaris@gmail.com>
2024-03-06 17:39:34 +0300

    spin and win jetx promo 	modified:   src/components/pages/promotions/Promo.js 	modified:   src/components/pages/promotions/PromoCards.js 	modified:   src/components/pages/promotions/promo.json

--- src/components/pages/promotions/PromoCards.js
+++ src/components/pages/promotions/PromoCards.js
@@ -16,7 +16,7 @@ const PromoCards = () => {
     const gaEventTracker = useAnalyticsEventTracker('Promotions');
     const user = getFromLocalStorage('user')
 
-    let ids = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11, 12, 13, 14,15,16,17,18,19,20,21];
+    let ids = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11, 12, 13, 14,15,16,17,18,19,20,21, 22];
     const navigate = useNavigate()
 
     let message = {status: 401, message: 'This Promotion is for new Users', token: ''};
@@ -41,6 +41,42 @@ const PromoCards = () => {
                     "row text-white pt-2 border-0 d-flex promo-container-profile d-flex align-self-start align-items-start"
                 }
             > 
+            <div className="col-md-2 promo-styling shadow-lg promotion">
+                    <div className="d-flex flex-column promo-inner">
+                        <img src={'https://cdn.betnare.com/carousel/Spin&WIn.webp'} className={"rounded promo-image "}/>
+                        <h5
+                            className="bold d-flex justify-content-center h4 pt-2"
+                            style={{color: "#ea5d0b"}}
+                        >
+                           SPIN & WIN ISTANT PRIZES WITH JETX!
+                        </h5>
+                        <p className="container-profile mx-1 px-2 text-data-promotions">
+                        Participating Games are Jetx, Balloon, PlinkoX, Multihot5, FootballX, CricketX, GeniesBonanza, SlicerX, JokerBuyBonus, Cappadocia, BurningICE40, Multihotways, JetX3, BlazingHot, BlazingHot10 ...                                          </p>
+                        <hr/>
+
+                        <div className="d-flex justify-content-between my-2 mx-2">
+
+                            <button className={"profile-button border-0 h-25 rounded promo-button"}
+                                    style={{background: "#ea5d0b"}} onClick={() => {
+                                gaEventTracker('spin_n_win');
+                                navigate(`/smart-play?game=JetX&category=JetX&status=live`);
+                                setUtmSouceCampaignOnPromotions('spin_n_win')
+                            }}>Play Jetx!
+                            </button>
+                            <div
+                                className={"d-flex  align-self-center   h-25 border-0 bg-transparent cursor-pointer"}
+                                style={{color: "#ea5d0b"}}
+                                onClick={() => {
+                                    navigate(`/promo?id=${ids[21]}`);
+                                    window.scrollTo(0, 0); // Scroll to the top of the page
+                                }}
+                            >
+                                Read More
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
             
             <div className="col-md-2 promo-styling shadow-lg promotion">
                     <div className="d-flex flex-column promo-inner">

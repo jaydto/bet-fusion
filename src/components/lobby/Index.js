50aeb41fc5bb520d656d9f68ad07f6f24439538d
jaydto <johnckaris@gmail.com>
2024-09-06 00:09:43 +0300

    modified:   src/components/lobby/Index.js 	modified:   src/components/pages/smart-soft/SmartPlay.js

--- src/components/lobby/Index.js
+++ src/components/lobby/Index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import CasinoGamesComponent from "./body";
 import CasinoLayout from "./casinoLayout";
 import { useParams } from "react-router-dom";
 import CasinoOptions from "./casinoOptions";
 import './index.css';
-
+import { useDispatch } from "react-redux";
+import {
+  setState as setVirtualGame,
+} from "../../redux/virtualsSlice";
 const CasinoIndex = () => {
   const { provider, category } = useParams();
+  const dispatch =useDispatch()
+
+
+  useEffect(() => {
+
+    return () => {
+      dispatch(setVirtualGame("casino_search", []));
+    }
+  
+    },[] )
 
   return (
     <CasinoLayout>

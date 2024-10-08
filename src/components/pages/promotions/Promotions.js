5e1abb8e8c8c520243b8992902d118ef4869ce45
jaydto <johnckaris@gmail.com>
2024-10-08 11:28:17 +0300

    modified:   src/components/pages/promotions/Promotions.js

--- src/components/pages/promotions/Promotions.js
+++ src/components/pages/promotions/Promotions.js
@@ -30,7 +30,7 @@ const Promotions = () => {
                                             </span>
                                     <div
                                         className=" shadow-sm d-flex justify-content-center col-8 p-2 shadow-sm promotion-header text-white">
-                                        BETNARE PROMOTIONS
+                                         PROMOTIONS
                                     </div>
                                 </div>
                                 <PromoCards/>

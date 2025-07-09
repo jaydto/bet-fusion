b0fbd7ea2dea525ae82165cbc57fecad73598294
Kennedy Wanjahi <kennedywanjahi@gmail.com>
2025-07-09 12:56:42 +0300

    Correct Endpoint for reset password

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -114,7 +114,7 @@ export const resetSubmitForm = createAsyncThunk(
 
     console.log("values", values)
 
-      const endpoint = '/v1/code?page=resetPass';
+      const endpoint = '/v1/code';
 
       const [status, response] = await makeRequest({
           url: endpoint,
